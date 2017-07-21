6d3d9f35e0e89311ad7953f014c827fa375f20e2
Daniel Lim <ddanielnp@hotmail.com>
2017-07-21 12:30:38 +0800

    add heroku config

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
-const url = 'mongodb://localhost:27017/placies'
+const url = process.env.MLAB_URI || 'mongodb://localhost:27017/placies'
 
 mongoose.Promise = global.Promise
 mongoose.connect(url, {
